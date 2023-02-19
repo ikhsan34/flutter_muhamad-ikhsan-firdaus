c4a8d6c15395a1a4235e55a360b0585f3d73c31a
Ikhsan <ikhsanfirdaus74@gmail.com>
2023-02-19 08:56:44 +0700

    Praktikum soal 1

---
+++ 6_Fundamental Dart/praktikum/soal1.dart
@@ -0,0 +1,15 @@
+// Soal prioritas 1
+void main() {
+  int panjang = 5;
+  int lebar = 3;
+
+  // Rumus luas dan keliling
+  int luas = panjang * lebar;
+  int keliling = (lebar * 2) + (panjang * 2);
+
+  // Print
+  print("Panjang : " + panjang.toString() + ", Lebar : " + lebar.toString());
+  print('Luas bangun datar: ' + luas.toString());
+  print('Keliling bangun datar: ' + keliling.toString());
+
+}
