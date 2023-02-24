6d7b60083ebc9a33a5be6e203003cd1bc2ccb9ba
Ikhsan <ikhsanfirdaus74@gmail.com>
2023-02-24 20:04:49 +0700

    Prioritas 1 - Async await
    File praktikum + screenshots

---
+++ 8_Advance Function - Async-Await - Collection/praktikum/prioritas1.dart
@@ -0,0 +1,21 @@
+void main() async {
+
+  List list1 = await pengali([1,2,3,4,5], 10);
+
+  print(list1);
+  
+}
+
+// Function pengali
+Future<List> pengali(List<int> listNum, int pengali) async {
+
+  List result = [];
+
+  for(var num in listNum) {
+    await Future.delayed(const Duration(seconds: 1), () {
+      result.add(num * pengali);
+    });
+  }
+
+  return result;
+}
