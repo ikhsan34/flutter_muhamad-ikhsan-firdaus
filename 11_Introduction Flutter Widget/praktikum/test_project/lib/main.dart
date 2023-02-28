21a471e806a3054e45340990491d24f401eef207
Ikhsan <ikhsanfirdaus74@gmail.com>
2023-02-28 08:48:47 +0700

    create flutter app - Introduction Flutter Widget

---
+++ 11_Introduction Flutter Widget/praktikum/test_project/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter demo'),
+      ),
+      body: const Center(
+        child: Text('This is body'),
+      ),
+    );
+  }
+}
