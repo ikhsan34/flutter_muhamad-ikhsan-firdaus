4bcd200f3983d4409f4b2919d422bd26798f9af3
Ikhsan <ikhsanfirdaus74@gmail.com>
2023-04-16 05:51:42 +0700

    Adding screenshots

--- 28_UI Testing/praktikum/ui_testing/test/widget_test.dart
+++ 28_UI Testing/praktikum/ui_testing/test/widget_test.dart
@@ -10,21 +10,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:ui_testing/main.dart';
 
 void main() {
-  testWidgets('Contacts App', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    // await tester.pumpWidget(const Dashboard());
 
-    // Verify that our counter starts at 0.
-    // expect(find.text('0'), findsOneWidget);
-    // expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
-
-    // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
+  // Halaman Dashboard
+  testWidgets('Contacts Dashboard', (WidgetTester tester) async {
 
     await tester.pumpWidget(const MyApp());
 
@@ -32,13 +20,20 @@ void main() {
     expect(find.text('No contact available, please create one'), findsOneWidget);
     expect(find.widgetWithIcon(FloatingActionButton, Icons.add), findsOneWidget);
     expect(find.widgetWithText(AppBar, 'Contacts'), findsOneWidget);
-    expect(find.widgetWithText(AppBar, 'Create Contact'), findsNothing);    
+    expect(find.widgetWithText(AppBar, 'Create Contact'), findsNothing);
 
-    // New Page
+  });
+
+  // Halaman Create Contact
+  testWidgets('Contacts Dashboard', (WidgetTester tester) async {
+
+    await tester.pumpWidget(const MyApp()); 
+
+    // Navigate
     await tester.tap(find.widgetWithIcon(FloatingActionButton, Icons.add));
     await tester.pumpAndSettle();
     expect(find.widgetWithText(AppBar, 'Create Contact'), findsOneWidget);
 
   });
-  
+
 }
