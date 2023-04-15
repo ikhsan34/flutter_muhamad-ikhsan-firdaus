c3cf9f0b3dae06e6e3c425dee8c57768a5c19340
Ikhsan <ikhsanfirdaus74@gmail.com>
2023-04-15 21:51:29 +0700

    UI Testing practice

--- 28_UI Testing/praktikum/ui_testing/lib/main.dart
+++ 28_UI Testing/praktikum/ui_testing/lib/main.dart
@@ -6,52 +6,63 @@ import 'package:ui_testing/screens/dashboard.dart';
 import 'package:ui_testing/screens/contact/contact_provider.dart';
 
 void main() {
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (_) => ContactProvider())
-    ],
-    child: MaterialApp(
-      theme: ThemeData().copyWith(
-        colorScheme: ThemeData().colorScheme.copyWith(secondary: const Color(0xff6650a5)),
-        appBarTheme: const AppBarTheme(
-          color: Color(0xff6650a5),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(const Color(0xff6650a5))
-          )
-        ),
-        inputDecorationTheme: const InputDecorationTheme(
-          filled: true,
-          fillColor: Color(0xffe6e0ed),
-          labelStyle: TextStyle(
-            color: Colors.grey
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ContactProvider())
+      ],
+      child: MaterialApp(
+        theme: ThemeData().copyWith(
+          colorScheme: ThemeData().colorScheme.copyWith(secondary: const Color(0xff6650a5)),
+          appBarTheme: const AppBarTheme(
+            color: Color(0xff6650a5),
           ),
-          focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.grey
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(const Color(0xff6650a5))
             )
           ),
-        )
-      ),
-      initialRoute: '/',
-      routes: {
-        '/':(context) => const Dashboard(),
-        //'/create_contact':(context) => const CreateContact()
-      },
-      onGenerateRoute: (settings) {
-        if (settings.name == '/create_contact') {
-          final args = settings.arguments as CreateContactArguments;
+          inputDecorationTheme: const InputDecorationTheme(
+            filled: true,
+            fillColor: Color(0xffe6e0ed),
+            labelStyle: TextStyle(
+              color: Colors.grey
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.grey
+              )
+            ),
+          )
+        ),
+        initialRoute: '/',
+        routes: {
+          '/':(context) => const Dashboard(),
+          //'/create_contact':(context) => const CreateContact()
+        },
+        onGenerateRoute: (settings) {
+          if (settings.name == '/create_contact') {
+            final args = settings.arguments as CreateContactArguments;
 
-          return MaterialPageRoute(
-            builder: (context) {
-              return CreateContactScreen(contactName: args.contactName, contactPhone: args.contactPhone);
-            },
-          );
-        }
-        return null;
-      },
-      // home: const MyApp(),
-    ),
-  ));
+            return MaterialPageRoute(
+              builder: (context) {
+                return CreateContactScreen(contactName: args.contactName, contactPhone: args.contactPhone);
+              },
+            );
+          }
+          return null;
+        },
+        // home: const MyApp(),
+      ),
+    );
+  }
 }
--- 28_UI Testing/praktikum/ui_testing/test/widget_test.dart
+++ 28_UI Testing/praktikum/ui_testing/test/widget_test.dart
@@ -7,23 +7,38 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ui_testing/screens/dashboard.dart';
+import 'package:ui_testing/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Contacts App', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Dashboard());
+    // await tester.pumpWidget(const Dashboard());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // expect(find.text('0'), findsOneWidget);
+    // expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // expect(find.text('0'), findsNothing);
+    // expect(find.text('1'), findsOneWidget);
+
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.text('Contacts'), findsOneWidget);
+    expect(find.text('No contact available, please create one'), findsOneWidget);
+    expect(find.widgetWithIcon(FloatingActionButton, Icons.add), findsOneWidget);
+    expect(find.widgetWithText(AppBar, 'Contacts'), findsOneWidget);
+    expect(find.widgetWithText(AppBar, 'Create Contact'), findsNothing);    
+
+    // New Page
+    await tester.tap(find.widgetWithIcon(FloatingActionButton, Icons.add));
+    await tester.pumpAndSettle();
+    expect(find.widgetWithText(AppBar, 'Create Contact'), findsOneWidget);
+
   });
+  
 }
